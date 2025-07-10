5fe9a6f1c9d6cac8560d86fc133d61287a102012
MedSveta <sveta1978medved@gmail.com>
2025-07-10 20:59:57 +0300

    Add, Get, Delete Tets

--- src/main/java/api_rest/CarController.java
+++ src/main/java/api_rest/CarController.java
@@ -7,6 +7,7 @@ import interfaces.BaseApi;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeSuite;
+
 import static io.restassured.RestAssured.given;
 
 public class CarController implements BaseApi {
@@ -23,20 +24,57 @@ public class CarController implements BaseApi {
                 .body(user)
                 .contentType(ContentType.JSON)
                 .when()
-                .post(BASE_URL+LOGIN_URL)
+                .post(BASE_URL + LOGIN_URL)
                 .thenReturn()
                 .getBody()
                 .as(TokenDto.class);
         System.out.println(tokenDto.getAccessToken());
     }
 
-    public Response addNewCar(CarDto car){
+    public Response addNewCar(CarDto car) {
         return given()
                 .body(car)
                 .contentType(ContentType.JSON)
                 .header("Authorization", tokenDto.getAccessToken())
                 .when()
-                .post(BASE_URL+ADD_NEW_CAR_URL)
+                .post(BASE_URL + ADD_NEW_CAR_URL)
+                .thenReturn();
+    }
+
+    public Response addNewCarNegative_WrongToken(CarDto car, String token) {
+        return given()
+                .body(car)
+                .contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .when()
+                .post(BASE_URL + ADD_NEW_CAR_URL)
+                .thenReturn();
+    }
+
+    public Response getUserCars() {
+        return given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", tokenDto.getAccessToken())
+                .when()
+                .get(BASE_URL + GET_ALL_USER_CARS_URL)
+                .thenReturn();
+    }
+
+    public Response getUserCarsNegative_WrongUrl(String url) {
+        return given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", tokenDto.getAccessToken())
+                .when()
+                .get(BASE_URL + url)
+                .thenReturn();
+    }
+
+    public Response deleteCarBySerialNumber(String serialNumber) {
+        return given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", tokenDto.getAccessToken())
+                .when()
+                .delete(BASE_URL + DELETE_CAR_URL + serialNumber)
                 .thenReturn();
     }
 }
